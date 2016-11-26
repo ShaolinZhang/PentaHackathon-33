d8f590ad236d9f555b23fc896b9f5efec8a12e72
Shaolin Zhang <shaolin@shaolinzhang.com>
2016-11-27 01:44:40 +0800

    Init Android Commit

---
+++ Android/app/src/main/java/shaolinzhang/com/penta_cv_ir/DescribeActivity.java
@@ -0,0 +1,241 @@
+package shaolinzhang.com.penta_cv_ir;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.microsoft.projectoxford.vision.VisionServiceClient;
+import com.microsoft.projectoxford.vision.VisionServiceRestClient;
+import com.microsoft.projectoxford.vision.contract.AnalysisResult;
+import com.microsoft.projectoxford.vision.contract.Category;
+import com.microsoft.projectoxford.vision.contract.Face;
+import com.microsoft.projectoxford.vision.contract.Tag;
+import com.microsoft.projectoxford.vision.contract.Caption;
+import com.microsoft.projectoxford.vision.rest.VisionServiceException;
+import shaolinzhang.com.penta_cv_ir.helper.ImageHelper;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Locale;
+
+public class DescribeActivity extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener{
+
+    // Flag to indicate which task is to be performed.
+    private static final int REQUEST_SELECT_IMAGE = 0;
+
+    // The button to select an image
+    private Button mButtonSelectImage;
+    private Button mTTSButton;
+
+    // The Text-To-Speech object init
+    private TextToSpeech mTextToSpeech;
+
+    // The URI of the image selected to detect.
+    private Uri mImageUri;
+
+    // The image selected to detect.
+    private Bitmap mBitmap;
+
+    // The edit to show status and result.
+    private TextView mEditText;
+
+    private VisionServiceClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_describe);
+        mTTSButton = (Button) findViewById(R.id.btnTTS);
+        mTTSButton.setOnClickListener(this);
+        mTextToSpeech = new TextToSpeech(this, this);
+
+        if (client==null){
+            client = new VisionServiceRestClient(getString(R.string.subscription_key));
+        }
+
+        mButtonSelectImage = (Button)findViewById(R.id.buttonSelectImage);
+        mEditText = (TextView) findViewById(R.id.textResult);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = mTextToSpeech.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(this, "Data lost or unsupported", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mTextToSpeech != null && !mTextToSpeech.isSpeaking()) {
+            mTextToSpeech.setPitch(0.5f);// 设置音调，值越大声音越尖（女生），值越小则变成男声,1.0是常规
+            mTextToSpeech.speak(mEditText.getText().toString(),
+                    TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_describe, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void doDescribe() {
+        mButtonSelectImage.setEnabled(false);
+        mEditText.setText("Describing...");
+
+        try {
+            new doRequest().execute();
+        } catch (Exception e)
+        {
+            mEditText.setText("Error encountered. Exception is: " + e.toString());
+        }
+    }
+
+    // Called when the "Select Image" button is clicked.
+    public void selectImage(View view) {
+        mEditText.setText("");
+
+        Intent intent;
+        intent = new Intent(DescribeActivity.this, shaolinzhang.com.penta_cv_ir.helper.SelectImageActivity.class);
+        startActivityForResult(intent, REQUEST_SELECT_IMAGE);
+    }
+
+    // Called when image selection is done.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("DescribeActivity", "onActivityResult");
+        switch (requestCode) {
+            case REQUEST_SELECT_IMAGE:
+                if(resultCode == RESULT_OK) {
+                    // If image is selected successfully, set the image URI and bitmap.
+                    mImageUri = data.getData();
+
+                    mBitmap = ImageHelper.loadSizeLimitedBitmapFromUri(
+                            mImageUri, getContentResolver());
+                    if (mBitmap != null) {
+                        // Show the image on screen.
+                        ImageView imageView = (ImageView) findViewById(R.id.selectedImage);
+                        imageView.setImageBitmap(mBitmap);
+
+                        // Add detection log.
+                        Log.d("DescribeActivity", "Image: " + mImageUri + " resized to " + mBitmap.getWidth()
+                                + "x" + mBitmap.getHeight());
+
+                        doDescribe();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private String process() throws VisionServiceException, IOException {
+        Gson gson = new Gson();
+
+        // Put the image into an input stream for detection.
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, output);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(output.toByteArray());
+
+        AnalysisResult v = this.client.describe(inputStream, 1);
+
+        String result = gson.toJson(v);
+        Log.d("result", result);
+
+        return result;
+    }
+
+    private class doRequest extends AsyncTask<String, String, String> {
+        // Store error message
+        private Exception e = null;
+
+        public doRequest() {
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            try {
+                return process();
+            } catch (Exception e) {
+                this.e = e;    // Store error
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String data) {
+            super.onPostExecute(data);
+            // Display based on error existence
+
+            mEditText.setText("");
+            if (e != null) {
+                mEditText.setText("Error: " + e.getMessage());
+                this.e = null;
+            } else {
+                Gson gson = new Gson();
+                AnalysisResult result = gson.fromJson(data, AnalysisResult.class);
+
+//                mEditText.append("Image format: " + result.metadata.format + "\n");
+//                mEditText.append("Image width: " + result.metadata.width + ", height:" + result.metadata.height + "\n");
+//                mEditText.append("\n");
+//
+//                for (Caption caption: result.description.captions) {
+//                    mEditText.append("Caption: " + caption.text + ", confidence: " + caption.confidence + "\n");
+//                }
+//                mEditText.append("\n");
+
+//                for (String tag: result.description.tags) {
+//                    mEditText.append("Tag: " + tag + "\n");
+//                }
+                for(int i = 0; i < 5; i++){
+                    String tag = result.description.tags.get(i);
+                    mEditText.append("" + tag + "\n");
+                }
+                mEditText.append("\n");
+
+//                mEditText.append("\n--- Raw Datza ---\n\n");
+//                mEditText.append(data);
+//                mEditText.setSelection(0);
+            }
+
+            mButtonSelectImage.setEnabled(true);
+        }
+    }
+}
